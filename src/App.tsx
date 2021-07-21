f3397283babc79e8823d7419a959823b9851aa49
ryanmstokes <ryanstoke@hotmail.com>
2021-07-21 14:02:12 +0700

    Rollback version of Material-UI package to avoid type warnings

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 console.log('dataProvider', dataProvider)
-const App = () =>
+const App: React.FunctionComponent = () =>
     <Admin dataProvider={dataProvider}>
         <Resource name="users" list={ListGuesser} />
     </Admin>
